709d76cfc0da45b864c9d01c91b192b96898a171
andrianazz <andrianwahyu41@gmail.com>
2023-08-23 10:50:43 +0700

    add in_app_update

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:in_app_update/in_app_update.dart';
 
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 
 void main() {
+  InAppUpdate.checkForUpdate().then((updateInfo) {
+    print(updateInfo);
+    if (updateInfo.updateAvailability == UpdateAvailability.updateAvailable) {
+      if (updateInfo.immediateUpdateAllowed) {
+        // Perform immediate update
+        InAppUpdate.performImmediateUpdate().then((appUpdateResult) {
+          if (appUpdateResult == AppUpdateResult.success) {
+            //App Update successful
+          }
+        });
+      } else if (updateInfo.flexibleUpdateAllowed) {
+        //Perform flexible update
+        InAppUpdate.startFlexibleUpdate().then((appUpdateResult) {
+          if (appUpdateResult == AppUpdateResult.success) {
+            //App Update successful
+            InAppUpdate.completeFlexibleUpdate();
+          }
+        });
+      }
+    }
+  });
+
   runApp(
     ScreenUtilInit(
       designSize: const Size(375, 812),
